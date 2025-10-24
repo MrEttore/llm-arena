79cf1fb964b9f994acf67837dda01b8b8917a80a
MrEttore <ettoremarangon@gmail.com>
2025-10-25 00:55:13 +0200

    feat: add LoadPresentsButton export to button components

--- src/ui/buttons/index.ts
+++ src/ui/buttons/index.ts
@@ -1,6 +1,6 @@
 export { default as AddButton } from "./AddButton";
 export { default as CancelButton } from "./CancelButton";
 export { default as ClearButton } from "./ClearButton";
+export { default as LoadPresentsButton } from "./LoadPresentsButton";
 export { default as ResetButton } from "./ResetButton";
 export { default as StartButton } from "./StartButton";
-export { default as LoadPresentsButton } from "./LoadPresentsButton";
