33ea499b63d766af3a82e2b8f5c47dda482a9e72
MrEttore <ettoremarangon@gmail.com>
2025-11-13 16:20:16 +0100

    style: update layout and styling of App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import { AppFooter, AppHeader } from "@/ui/layout";
 
 export default function App() {
   return (
-    <div className="flex h-dvh flex-col">
+    <div className="flex h-dvh flex-col text-white">
       <AppHeader />
-      <main className="flex flex-1 flex-col overflow-hidden bg-white/5 backdrop-blur-xl">
-        <div className="grid min-h-0 flex-1 grid-cols-2 lg:px-1 2xl:px-5">
+      <main className="flex min-h-0 flex-1 flex-col px-3 py-4 backdrop-blur sm:px-4 lg:px-6 xl:px-8">
+        <div className="grid min-h-0 flex-1 grid-cols-1 gap-4 lg:grid-cols-2">
           <Settings />
           <ChatLayout>
             {/* <ChatHeader /> */}
