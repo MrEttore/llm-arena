462392bddb8666c04aa093e9b70718eaa6ec0dfd
MrEttore <ettoremarangon@gmail.com>
2025-11-13 16:53:23 +0100

    style: update button styles for consistency across CancelButton, ResetButton, and StartButton components

--- src/ui/buttons/ResetButton.tsx
+++ src/ui/buttons/ResetButton.tsx
@@ -11,7 +11,7 @@ export default function ResetButton({ disabled = false, onClick }: Props) {
     <button
       type="button"
       onClick={onClick}
-      className={`flex items-center gap-1 rounded-lg border-1 border-transparent px-2 py-1 font-semibold tracking-wider text-white transition-all duration-300 lg:text-xs 2xl:text-sm ${
+      className={`flex items-center gap-1 rounded-xl border-1 border-transparent px-2 py-1 font-semibold tracking-wider text-white transition-all duration-300 lg:text-sm 2xl:text-base ${
         disabled
           ? "cursor-not-allowed opacity-50"
           : "cursor-pointer hover:border-white/5 hover:bg-white/5 hover:shadow-xs active:scale-[98%]"
